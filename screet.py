8325febd704d4607836d78aa3505aa59faa10dff
melonfacedoom <mmjacobmm@gmail.com>
2021-12-07 10:01:40 -0500

    continuation rejiggered

--- screet.py
+++ screet.py
@@ -11,8 +11,9 @@ UniqueViolation = errors.lookup('23505')
 
 SEARCH_TERMS = ["covid", "corona", "coronavirus", "corona virus", "covid-19", "pandemic", "plandemic", "lockdown",
                 "virus", "vaccine"]
-SINCE = os.environ.get("SCREET_SINCE", "2021-01-01")
-UNTIL = os.environ.get("SCREET_UNTIL", "2021-01-31")
+LIMIT = os.environ.get("SCREET_LIMIT", 1000)
+SINCE = os.environ.get("SCREET_SINCE", "2021-05-30")
+UNTIL = os.environ.get("SCREET_UNTIL", "2021-05-30")
 SINCE = datetime.datetime.strptime(SINCE, "%Y-%m-%d")
 UNTIL = datetime.datetime.strptime(UNTIL, "%Y-%m-%d")
 
@@ -22,37 +23,67 @@ def main():
     pass
 
 @main.command()
-@click.argument('limit', type=click.INT)
-def scrape(limit):
+def scrape():
     conn = establish_connection()
     make_table(conn)
     col_names = get_col_names(conn, "tweets")
-    latest_tweet = get_tweets(conn, order="DESC", number_of_tweets=1)
-    if latest_tweet:
-        latest_tweet = latest_tweet[0]
-        day = latest_tweet['date'].strftime("%Y-%m-%d")
-        min_date = datetime.datetime.strptime(day + "-00:00:00", "%Y-%m-%d-%H:%M:%S")
-        max_date = min_date + datetime.timedelta(days=1)
-        oldest_tweet = get_tweets(conn, order="ASC", number_of_tweets=1, min_date=min_date, max_date=max_date)[0]
+    oldest_tweet = get_tweets(conn, order="ASC", number_of_tweets=1, min_date=SINCE, max_date=UNTIL)
+    if oldest_tweet:
+        oldest_tweet = oldest_tweet[0]
         start_dbcount = get_dbcount()
-        begin_scraping(conn=conn, col_names=col_names, max_id=oldest_tweet['id'], since=latest_tweet['date'],
-                       limit=limit)
+        begin_scraping(conn=conn, col_names=col_names, max_id=oldest_tweet['id'], until=oldest_tweet['date'])
         end_dbcount = get_dbcount()
         if start_dbcount == end_dbcount:
             # band-aid for an issue. Twitter returns tweets starting with the end of the day.
             # this means max-id can be used to pick up where scraping left off mid day
-            # however, once the next day is reached, max-id will prevent any new tweets from being scraped
+            # however, once a day is completed, max-id will prevent the next day from being accessed.
             # therefore, scraping should be re-initiated with no max-id
-            begin_scraping(conn=conn, col_names=col_names, since=latest_tweet['date'],
-                       limit=limit)
+            begin_scraping(conn=conn, col_names=col_names, until=oldest_tweet['date'])
     else:
-        begin_scraping(conn=conn, col_names=col_names, limit=limit)
+        begin_scraping(conn=conn, col_names=col_names)
     conn.close()
 
 
 @main.command()
 def dbcount():
     print(get_dbcount())
+    
+    
+@main.command()
+def monthly():
+    month_counts = get_count_by_month()
+    print(month_counts)
+    
+    
+@main.command()
+def daily():
+    day_counts = get_count_by_day()
+    print(day_counts)
+    
+    
+def get_count_by_month():
+    conn = establish_connection()
+    cur = conn.cursor(cursor_factory=DictCursor)
+    cur.execute(f'''SELECT date_trunc('month', date) AS month, count(id) as monthly_count
+                    FROM tweets
+                    GROUP BY month''')
+    month_counts = cur.fetchall()
+    cur.close()
+    conn.close()
+    return month_counts
+
+
+def get_count_by_day():
+    conn = establish_connection()
+    cur = conn.cursor(cursor_factory=DictCursor)
+    cur.execute(f'''SELECT date_trunc('day', date) AS day, count(id) as daily_count
+                    FROM tweets
+                    GROUP BY day''')
+    day_counts = cur.fetchall()
+    cur.close()
+    conn.close()
+    return day_counts
+    
 
 def get_dbcount():
     conn = establish_connection()
@@ -120,15 +151,18 @@ def get_tweets(conn, order="DESC", number_of_tweets=1, min_date=None, max_date=N
     return tweets
 
 
-def begin_scraping(conn, col_names, limit, search_terms=None, since=SINCE, until=UNTIL, max_id=None):
+def begin_scraping(conn, col_names, search_terms=None, limit=LIMIT, since=SINCE, until=UNTIL, max_id=None):
     if search_terms is None:
         search_terms = SEARCH_TERMS
 
-    search_terms_string = " OR ".join(search_terms)
-    keywords_string = "since:" + since.strftime("%Y-%m-%d-%H:%M:%S") + " until:" + until.strftime("%Y-%m-%d-%H:%M:%S")
+    search_terms_string = " OR ".join(search_terms) # i.e. "covid OR corona OR pandemic"
+    until = until + datetime.timedelta(days=1) # until tag seems not to be inclusive
+    keywords_string = "since:" + since.strftime("%Y-%m-%d") + " until:" + until.strftime("%Y-%m-%d")
+    # i.e. "since:2021-01-01 until:2021-12-01"
     if max_id:
         keywords_string += " max_id:" + str(max_id)
     query = search_terms_string + " " + keywords_string
+    print(query)
 
     for i, tweet in enumerate(
             sntwitter.TwitterSearchScraper(query=query).get_items()):
